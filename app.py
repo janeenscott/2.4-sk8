c4fa18ce5331bc84cb90e12d035f607f71aa74bb
Janeen Scott <janeen.e.scott@gmail.com>
2019-02-06 19:43:10 -0500

    added Procfile, attempted templating

--- app.py
+++ app.py
@@ -7,10 +7,24 @@ import requests
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    response = requests.get('https://swapi.co/api/planets/')
+@app.route('/thank-you/', methods=['GET', 'POST'])
+# configured to accept POST requests, so we can access for data (name and email) as a dictionary
+def thank_you():
+    print(request.form[''])
+    return 'Thank you!'
 
-    planets = data['results']
 
-    return render_template('index.html', planet_list=planets)
+
+
+
+
+
+#********** from planets demo
+#need to switch planet api info info form
+# @app.route("/")
+# def index():
+#     response = requests.get('https://swapi.co/api/planets/')
+#
+#     planets = data['results']
+#
+#     return render_template('index.html', planet_list=planets)
