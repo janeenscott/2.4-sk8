a4ffa1137cc1ee322f57dba4e61ba9d91ae4d754
Janeen <janeen.e.scott@gmail.com>
2019-03-04 21:10:20 -0500

    fixed template issue

--- app.py
+++ app.py
@@ -7,6 +7,21 @@ import requests
 app = Flask(__name__)
 
 
+@app.route('/')
+def index():
+
+    return render_template('index.html')
+
+#********** from planets demo
+#need to switch planet api info info form
+# @app.route("/")
+# def index():
+#     response = requests.get('https://swapi.co/api/planets/')
+#
+#     planets = data['results']
+#
+# #     return render_template('index.html', planet_list=planets)
+
 @app.route('/thank-you/', methods=['GET', 'POST'])
 # configured to accept POST requests, so we can access for data (name and email) as a dictionary
 def thank_you():
@@ -18,13 +33,3 @@ def thank_you():
 
 
 
-
-#********** from planets demo
-#need to switch planet api info info form
-# @app.route("/")
-# def index():
-#     response = requests.get('https://swapi.co/api/planets/')
-#
-#     planets = data['results']
-#
-#     return render_template('index.html', planet_list=planets)
